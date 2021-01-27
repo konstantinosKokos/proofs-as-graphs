fbeb7d5309d16cddad69cbe44786cf39ecec04ac
konstantinosKokos <konstantinos@riseup.net>
2021-01-27 17:22:18 +0100

    move type declaration imports

---
+++ Graphs/typing.py
@@ -0,0 +1,9 @@
+import torch
+from torch import Tensor
+from torch_geometric.data import Data, Batch, DataLoader
+from torch.optim.optimizer import Optimizer
+from torch.nn import Module
+
+from typing import List, Tuple, Union
+
+from numpy import array
