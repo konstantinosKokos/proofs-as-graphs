2203cbb18e0fb7d90b1a57a3b6767201e0db78e5
konstantinosKokos <konstantinos@riseup.net>
2021-01-27 17:21:28 +0100

    remove last century stuff

--- Graphs/neural/embedding.py
+++ Graphs/neural/embedding.py
@@ -1,25 +1,24 @@
-import torch
+from ..typing import array, Union, Tensor, Module
+from torch import rand, tensor, float32
+from torch.nn import Parameter, Embedding
 from torch.nn.functional import embedding, linear
-from torch.nn import Module, Parameter, Embedding
-from typing import Union
-from numpy import array
 
 
 class InvertibleEmbedder(Module):
     def __init__(self, num_embeddings: int, embedding_dim: int, padding_idx: int = 0):
         super(InvertibleEmbedder, self).__init__()
-        self.table = Parameter(data=torch.rand(num_embeddings, embedding_dim), requires_grad=True)
+        self.table = Parameter(data=rand(num_embeddings, embedding_dim), requires_grad=True)
         self.padding_idx = padding_idx
 
-    def embed(self, ids: torch.Tensor) -> torch.Tensor:
+    def embed(self, ids: Tensor) -> Tensor:
         return embedding(ids, self.table, self.padding_idx)
 
-    def invert(self, weights: torch.Tensor) -> torch.Tensor:
+    def invert(self, weights: Tensor) -> Tensor:
         return linear(weights, self.table.t())
 
 
-def from_table(table: Union[array, torch.Tensor]) -> Embedding:
+def from_table(table: Union[array, Tensor]) -> Embedding:
     ne, ed = table.shape
     embedder = Embedding(ne, ed, padding_idx=0)
-    embedder.weight.data = torch.tensor(table).to(torch.float)
+    embedder.weight.data = tensor(table).to(float32)
     return embedder
