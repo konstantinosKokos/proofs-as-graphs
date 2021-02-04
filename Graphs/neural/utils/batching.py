8685402787a112a70e96b946d6c2a0e612bcdc43
konstantinosKokos <konstantinos@riseup.net>
2021-02-04 10:15:55 +0100

    add pair batching functionality

--- Graphs/neural/utils/batching.py
+++ Graphs/neural/utils/batching.py
@@ -1,17 +1,47 @@
-from ...typing import List, Tuple, Data, DataLoader
+from ...typing import List, Tuple, Data, DataLoader, Tensor
 from torch_geometric.utils import to_undirected, add_self_loops
 import torch
 
 
-def graph_to_data(atom_ids: List[int], word_ids: List[int], edge_index: Tuple[List[int], List[int]]) -> Data:
+def tensorize_graph(atom_ids: List[int], word_ids: List[int], edge_index: Tuple[List[int], List[int]]) \
+        -> Tuple[Tensor, Tensor, Tensor]:
     atom_ids = torch.tensor(atom_ids, dtype=torch.long)
     word_ids = torch.tensor(word_ids, dtype=torch.long)
-    data = Data(x=torch.stack((atom_ids, word_ids), dim=-1),
-                edge_index=to_undirected(torch.tensor(edge_index, dtype=torch.long)),
-                y=atom_ids.unsqueeze(-1))
-    return data
+    return (torch.stack((atom_ids, word_ids), dim=-1),
+            to_undirected(torch.tensor(edge_index, dtype=torch.long)),
+            atom_ids.unsqueeze(-1))
 
 
-def make_dataloader(data_list: List[Data], batch_size: int) -> DataLoader:
+
+def graph_to_data(atom_ids: List[int], word_ids: List[int], edge_index: Tuple[List[int], List[int]]) -> Data:
+    x, edge_index, y = tensorize_graph(atom_ids, word_ids, edge_index)
+    return Data(x=x, edge_index=edge_index, y=y)
+
+
+def graph_loader(data_list: List[Data], batch_size: int) -> DataLoader:
     return DataLoader(data_list, batch_size=batch_size)
 
+
+class GraphPair(Data):
+    def __init__(self, edge_index_h: Tensor, x_h: Tensor, edge_index_p: Tensor, x_p: Tensor, y: Tensor):
+        super(GraphPair, self).__init__()
+        self.edge_index_h = edge_index_h
+        self.x_h = x_h
+        self.edge_index_p = edge_index_p
+        self.x_p = x_p
+        self.y = y
+
+    def __inc__(self, key, value):
+        if key == 'edge_index_h':
+            return self.x_h.shape(0)
+        if key == 'edge_index_p':
+            return self.x_p.shape(0)
+        return super(GraphPair, self).__inc__(key, value)
+
+
+def graphs_to_data(graph_a: Tuple[List[int], List[int], Tuple[List[int], List[int]]],
+                   graph_b: Tuple[List[int], List[int], Tuple[List[int], List[int]]],
+                   label: int) -> GraphPair:
+    x_h, edge_index_h, _ = tensorize_graph(*graph_a)
+    x_p, edge_index_p, _ = tensorize_graph(*graph_b)
+    return GraphPair(edge_index_h, x_h, edge_index_p, x_p, torch.tensor(label, dtype=torch.long))
